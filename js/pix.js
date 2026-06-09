95d27fadcfb2723a12e96bf229d6bd0fb9dfb67c
Benqx00 <thomazcandelaria58@gmail.com>
2026-06-09 14:29:11 -0300

    deixa o site top

---
+++ js/pix.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const total = localStorage.getItem("totalPedido") || 0;
+
+    document.getElementById("valorPix").textContent =
+        `R$ ${parseFloat(total).toFixed(2).replace(".", ",")}`;
+
+});
